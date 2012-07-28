b111311dd96408a737034523c84d07c08e4798c0
kinogam <kinogam@gmail.com>
2012-07-28 17:56:23 +0800

    update

--- Chart.js
+++ Chart.js
@@ -1,10 +1,19 @@
 ﻿(function (window) {
+    var amount = 0;
+
     var c = {
         add: function (goods) {
-
+            if (Object.prototype.toString.call(goods) === '[object Array]') {
+                for (var i = 0; i < goods.length; i++) {
+                    amount += goods[i].price * goods[i].number;
+                }
+            }
+            else {
+                amount += goods.price * goods.number;
+            }
         },
         totalPrice: function () {
-            return 300;
+            return amount;
         }
     };
 
